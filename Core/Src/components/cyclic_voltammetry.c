f2ac8d6827cf6b2d057b78a2d76b143b5df3751a
adriapladevall <apladevall@werfen.com>
2023-06-10 12:10:24 +0200

    Voltammetry with cycles.

--- Core/Src/components/cyclic_voltammetry.c
+++ Core/Src/components/cyclic_voltammetry.c
@@ -41,92 +41,96 @@ void cyclic_voltammetry(struct CV_Configuration_S cvConfiguration){
 
 	Start_Timer(SamplingPeriod);
 
-	uint8_t numbercycles = 0;
-
-	eStep=cvConfiguration.eStep;
-
 	uint32_t EllapsedTime = 0;
 	uint32_t counter_data = 1;
+	uint8_t numbercycles = 0;
+
 
 
 	while (EllapsedTime < SamplingPeriod){
+		while (numbercycles < cycles){
 
-			if (TimeoutEllapsed()){
-				EllapsedTime = EllapsedTime + SamplingPeriod;
+					if (TimeoutEllapsed()){
+						EllapsedTime = EllapsedTime + SamplingPeriod;
 
-				ADC_Start();
-				uint32_t voltageAdc = ADC_get_Voltage();
-				uint32_t currentAdc = ADC_get_Current();
+						ADC_Start();
+						uint32_t voltageAdc = ADC_get_Voltage();
+						uint32_t currentAdc = ADC_get_Current();
 
-				double current = calculateIcellCurrent(currentAdc);
-				double voltage = calculateVrefVoltage(voltageAdc);
-				ADC_Stop();
+						double current = calculateIcellCurrent(currentAdc);
+						double voltage = calculateVrefVoltage(voltageAdc);
+						ADC_Stop();
 
-				struct Data_S data;
+						struct Data_S data;
 
-				data.point = counter_data;
-				counter_data = counter_data +1;
-				data.timeMs = EllapsedTime;
-				data.current = current;
-				data.voltage = voltage;
+						data.point = counter_data;
+						counter_data = counter_data +1;
+						data.timeMs = EllapsedTime;
+						data.current = current;
+						data.voltage = voltage;
 
-				MASB_COMM_S_sendData(data);
+						MASB_COMM_S_sendData(data);
 
-				ClearTimeout();
+						ClearTimeout();
 
-					if (compare_function(VObjective, cvConfiguration.eVertex1)) {
+							if (compare_function(VObjective, cvConfiguration.eVertex1)) {
 
-						VObjective = cvConfiguration.eVertex2;
+								VObjective = cvConfiguration.eVertex2;
 
-					} else if (compare_function(VObjective, cvConfiguration.eVertex2)) {
+							} else if (compare_function(VObjective, cvConfiguration.eVertex2)) {
 
-						VObjective = cvConfiguration.eBegin;
+								VObjective = cvConfiguration.eBegin;
 
-					} else {
-						VObjective = cvConfiguration.eVertex1;
+							} else if (compare_function(VObjective, cvConfiguration.eBegin)) {
 
+								VObjective = cvConfiguration.eVertex1;
+								// AQUÍ LI HAURÍEM DE SUMAR UN CICLE I COMENCEM UN NOU CICLE
+								numbercycles = numbercycles + 1; // We start a new cycle
+
+							}
+
+
+							} else {
+								if (compare_function(VObjective, cvConfiguration.eVertex1)) {
+
+									if (cvConfiguration.eBegin + eStep > VObjective) {
+
+										cvConfiguration.eBegin = VObjective;
+
+									} else {
+
+										cvConfiguration.eBegin = cvConfiguration.eBegin + eStep;
+									}
+								}
+								if (compare_function(VObjective, cvConfiguration.eVertex2)) {
+
+									if (cvConfiguration.eBegin - eStep < VObjective) {
+
+										cvConfiguration.eBegin = VObjective;
+
+									} else {
+
+										cvConfiguration.eBegin = cvConfiguration.eBegin - eStep;
+									}
+								}
+								if (compare_function(VObjective, cvConfiguration.eBegin)) {
+
+									if (cvConfiguration.eBegin + eStep > VObjective) {
+
+										cvConfiguration.eBegin = VObjective;
+
+									} else {
+
+										cvConfiguration.eBegin = cvConfiguration.eBegin + eStep;
+
+									}
+								}
+							}
+
+						MCP4725_SetOutputVoltage(hdac, calculateDacOutputVoltage(cvConfiguration.eBegin));
 
 					}
-					} else {
-						if (compare_function(VObjective, cvConfiguration.eVertex1)) {
-
-							if (cvConfiguration.eBegin + eStep > VObjective) {
-
-								cvConfiguration.eBegin = VObjective;
-
-							} else {
-
-								cvConfiguration.eBegin = cvConfiguration.eBegin + eStep;
-							}
-						}
-						if (compare_function(VObjective, cvConfiguration.eVertex2)) {
-
-							if (cvConfiguration.eBegin - eStep < VObjective) {
-
-								cvConfiguration.eBegin = VObjective;
-
-							} else {
-
-								cvConfiguration.eBegin = cvConfiguration.eBegin - eStep;
-							}
-						}
-						if (compare_function(VObjective, cvConfiguration.eBegin)) {
-
-							if (cvConfiguration.eBegin + eStep > VObjective) {
-
-								cvConfiguration.eBegin = VObjective;
-
-							} else {
-
-								cvConfiguration.eBegin = cvConfiguration.eBegin + eStep;
-
-							}
-						}
-					}
-
-				MCP4725_SetOutputVoltage(hdac, calculateDacOutputVoltage(cvConfiguration.eBegin));
-
-			}
+	}
 
 			Close_Rele();
 
---
+++ Release/Core/Src/components/ad5280_driver.d
@@ -0,0 +1,4 @@
+Core/Src/components/ad5280_driver.o: \
+ ../Core/Src/components/ad5280_driver.c \
+ ../Core/Inc/components/ad5280_driver.h
+../Core/Inc/components/ad5280_driver.h:
